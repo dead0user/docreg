7376d5793231c99e2459ad1d78ee2dfefa9819f2
dead0user <dead0user@gmail.com>
2021-10-22 12:34:10 +0200

    GUI
    Pierwszy, w miarę kompletny, interfejs
    Jeszcze bez wyświetlanej listy dokumentów

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 from peewee import *
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 
 db = SqliteDatabase('documents.db')
 
 
-class Company(Model):
-    name = CharField()
-
-    class Meta():
-        database = db
-
-class shortDate(Model):
-    name = CharField()
-
-    class Meta():
-        database = db
-
 class Document(Model):
     date = DateField()
     netto_quota = FloatField()
     doc_number = CharField()
-    company = ForeignKeyField(Company, backref='documents')
-    if_short_date = ForeignKeyField(shortDate)
+    company = CharField()
+    if_short_date = CharField(max_length=25)
     if_beer = FloatField()
     if_wine = FloatField()
     if_vodka = FloatField()
@@ -30,7 +21,60 @@ class Document(Model):
         database = db
 
 db.connect()
-db.create_tables([Company, shortDate, Document])
+db.create_tables([Document])
 
-add_company = Company(name='Eurocash')
-add_company.save()
+
+
+class mainWindow(Gtk.Window):
+
+    def __init__(self) -> None:
+        Gtk.Window.__init__(self, title='Rejestr dokumentów')
+        self.main_grid()
+
+    def main_grid(self) -> None:
+        self.grid = Gtk.Grid()
+        self.grid.set_column_spacing(5)
+        self.grid.set_row_spacing(5)
+        self.add(self.grid)
+
+        self.doc_date = Gtk.Entry()
+        self.grid.attach(self.doc_date, 0, 0, 1, 1)
+
+        self.quota = Gtk.Entry()
+        self.grid.attach(self.quota, 1, 0, 1, 1)
+
+        self.doc_number = Gtk.Entry()
+        self.grid.attach(self.doc_number, 2, 0, 1, 1)
+
+        self.doc_company = Gtk.Entry()
+        self.grid.attach(self.doc_company, 3, 0, 1, 1)
+
+        self.options = ['brak', 'nabiał', 'wędliny']
+        self.is_short_date = Gtk.ComboBoxText()
+        self.is_short_date.set_entry_text_column(0)
+        for list_item in self.options:
+            self.is_short_date.append_text(list_item)
+        self.grid.attach(self.is_short_date, 0, 1, 1, 1)
+
+        self.is_beer_quota = Gtk.Entry()
+        self.grid.attach(self.is_beer_quota, 1, 1, 1, 1)
+
+        self.is_wine_quota = Gtk.Entry()
+        self.grid.attach(self.is_wine_quota, 2, 1, 1, 1)
+
+        self.is_vodka_quota = Gtk.Entry()
+        self.grid.attach(self.is_vodka_quota, 3, 1, 1, 1)
+
+        self.add_document = Gtk.Button.new_with_mnemonic("_Dodaj")
+        self.grid.attach(self.add_document, 2, 2, 1, 1)
+
+        self.quit_from_app = Gtk.Button.new_with_mnemonic("_Wyjdź")
+        self.grid.attach(self.quit_from_app, 3, 2, 1, 1)
+
+
+
+if __name__ == "__main__":
+    win = mainWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
